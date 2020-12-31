7929e184065867e638225cdc317aec24ec4a7823
Pham Tuan <phamtuan199911@gmail.com>
2020-12-31 21:11:32 +0700

    Terminate prettier Add AppBar Add Home Add React Router Add Material UI/Icons

---
+++ src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import AppBar from "../../components/AppBar";
+
+const Home = () => {
+    return (
+        <div>
+            <AppBar/>
+        </div>
+    );
+}
+
+export default Home;
