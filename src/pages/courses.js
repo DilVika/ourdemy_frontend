310161b340462de341f64ca02ee04c6dab78e97a
Cao Đình Vĩ <cao.d.vi@gmail.com>
2021-01-10 15:55:09 +0700

    change message endpage

--- src/pages/courses.js
+++ src/pages/courses.js
@@ -45,19 +45,9 @@ const Courses = (props) => {
         loader={<h4>Loading...</h4>}
         endMessage={
           <p style={{ textAlign: "center" }}>
-            <b>Yay! You have seen it all</b>
+            <b>----- End -----</b>
           </p>
         }
-        // below props only if you need pull down functionality
-        //refreshFunction={this.refresh}
-        // pullDownToRefresh
-        // pullDownToRefreshThreshold={50}
-        // pullDownToRefreshContent={
-        //   <h3 style={{ textAlign: "center" }}>&#8595; Pull down to refresh</h3>
-        // }
-        // releaseToRefreshContent={
-        //   <h3 style={{ textAlign: "center" }}>&#8593; Release to refresh</h3>
-        // }
       >
         {coursesList}
       </InfiniteScroll>
