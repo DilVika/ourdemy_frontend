14599d45835033532e67ec3e146893aef38e2936
Pham Tuan <phamtuan199911@gmail.com>
2021-01-10 21:20:32 +0700

    Fix bugs Move fetch cats to each pageframe

--- src/index.js
+++ src/index.js
@@ -3,17 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {fetchCategories} from "./store/category";
-import store from "./store";
 import {initialState} from "./store/authen";
-import jwtDecode from "jwt-decode";
 
 require('dotenv').config()
 ReactDOM.render(
     <App/>,
     document.getElementById('root')
 );
-store.dispatch(fetchCategories())
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
