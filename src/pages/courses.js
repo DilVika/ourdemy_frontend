108251b5de3dd069a3d56e404c3480c47ff3b290
Cao Đình Vĩ <cao.d.vi@gmail.com>
2021-01-10 15:52:18 +0700

    add infinity load

--- src/pages/courses.js
+++ src/pages/courses.js
@@ -1,66 +1,23 @@
 import PageFrame from "../components/PageFrame";
-import React from "react";
+import React, { useState } from "react";
 import InforCard from "../components/InforCard";
+import InfiniteScroll from "react-infinite-scroll-component";
 
-const dummyInfo = [
-  {
-    src: "https://picsum.photos/300/200",
-    title: "Java Code Giấy",
-    kind: "Web Devevlopment",
-    author: "Ba Tê",
-    rate: 5,
-    count: "700",
-    currentPrice: "50",
-    originPrice: "70",
-    brief: "Java code giay",
-  },
-  {
-    src: "https://picsum.photos/300/200",
-    title: "Java Code Giấy",
-    kind: "Web Devevlopment",
-    author: "Ba Tê",
-    rate: 1,
-    count: "700",
-    currentPrice: "1",
-    originPrice: "70",
-    brief: "Java code giay",
-  },
-  {
-    src: "https://picsum.photos/300/200",
-    title: "Java Code Giấy",
-    kind: "Web Devevlopment",
-    author: "Ba Tê",
-    rate: 2,
-    count: "700",
-    currentPrice: "1",
-    originPrice: "70",
-    brief: "Java code giay",
-  },
-  {
-    src: "https://picsum.photos/300/200",
-    title: "Java Code Giấy",
-    kind: "Web Devevlopment",
-    author: "Ba Tê",
-    rate: 5,
-    count: "1",
-    currentPrice: "0",
-    originPrice: "70",
-    brief: "Java code giay",
-  },
-  {
-    src: "https://picsum.photos/300/200",
-    title: "Java Code Giấy",
-    kind: "Web Devevlopment",
-    author: "Ba Tê",
-    rate: 4.5,
-    count: "1",
-    currentPrice: "0",
-    originPrice: "70",
-    brief: "Java code giay",
-  },
-].map((val, index) => (
+
+const dummyInfo = Array.from({ length: 20 }, (v, i) => ({
+  src: "https://picsum.photos/300/200",
+  title: "Java Code Giấy",
+  kind: "Web Devevlopment",
+  author: "Ba Tê",
+  rate: 5,
+  count: "700",
+  currentPrice: "50" + i,
+  originPrice: "70" + i,
+  brief: "Java code giay",
+})).map((val, index) => (
   <InforCard
     key={index}
+    style={{margin: "10px"}}
     src={val.src}
     title={val.title}
     author={val.author}
@@ -73,9 +30,37 @@ const dummyInfo = [
 ));
 
 const Courses = (props) => {
+  const [coursesList, setCoursesList] = useState(dummyInfo);
+  const handleFetch = () => {
+    setCoursesList([...coursesList, dummyInfo]);
+  };
+
   return (
     <PageFrame>
-      {dummyInfo}
+      <h2>Courses</h2>
+      <InfiniteScroll
+        dataLength={coursesList.length} //This is important field to render the next data
+        next={handleFetch}
+        hasMore={true}
+        loader={<h4>Loading...</h4>}
+        endMessage={
+          <p style={{ textAlign: "center" }}>
+            <b>Yay! You have seen it all</b>
+          </p>
+        }
+        // below props only if you need pull down functionality
+        //refreshFunction={this.refresh}
+        // pullDownToRefresh
+        // pullDownToRefreshThreshold={50}
+        // pullDownToRefreshContent={
+        //   <h3 style={{ textAlign: "center" }}>&#8595; Pull down to refresh</h3>
+        // }
+        // releaseToRefreshContent={
+        //   <h3 style={{ textAlign: "center" }}>&#8593; Release to refresh</h3>
+        // }
+      >
+        {coursesList}
+      </InfiniteScroll>
     </PageFrame>
   );
 };
